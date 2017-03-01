29798f3ab5c275eb8cc076ec42f48878932950dc
Dan Mazo <danmazo32@gmail.com>
2017-03-01 17:41:22 +0800

    Implementing mario.hard, it was exhausting

---
+++ mario_hard.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h> //CS50 library to assist beginners for programming in C
+
+int main(void){
+    int height;
+    
+    do{
+        printf("Height: ");
+        height = get_int();
+    }while (height < 0);
+    
+    
+    int st = 2; //min stair limit
+    for(int i = height; i > 0; i--){
+        //for the spaces
+        for(int sp = 0; sp < i - 1; sp++){
+            printf(" ");
+        }
+        //for the hashes or stairs as you want to call it
+        /*
+            what this does is initialize st as h, then taking it down to Zero
+            for some reason. initializing it 
+        
+        */
+        
+        for(int h = st; h > 0; h--){
+            printf("#");    //this is the stairs
+        }
+        
+        //increasing stair limit
+        st++;
+        //adding new line, C does not have an endl syntax
+        printf("\n");
+    }
+    
+    //This algorithm may not be optimized but this would do, still a beginner though
+}
